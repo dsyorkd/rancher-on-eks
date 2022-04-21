fcde2a8d536f2e959436c97711d9a1dcec2e6bac
Spencer York <syork@yorkserv.com>
2022-04-21 08:23:49 -0500

    removed data call

--- 01_eks.tf
+++ 01_eks.tf
@@ -1,36 +1,3 @@
-data "aws_vpc" "vpc" {
-  count = var.vpc_create == 0 ? 1 : 0
-  id = var.vpc_create == 0 ? module.vpc.vpc_id : var.vpc_id
-}
-
-data "aws_availability_zones" "available" {
-}
-
-data "aws_subnets" "cluster_subnet_set" {
-  count = length(var.subnet_name_filters_for_cluster)
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.vpc[0].id]
-  }
-  filter {
-    name   = "tag:Name"
-    values = [var.subnet_name_filters_for_cluster[count.index]]
-  }
-}
-
-data "aws_subnets" "node_subnet_set" {
-  count = length(var.subnet_name_filters_for_nodes)
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.vpc[0].id]
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = [var.subnet_name_filters_for_nodes[count.index]]
-  }
-}
-
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
@@ -47,10 +14,9 @@ module "eks" {
   source          = "github.com/terraform-aws-modules/terraform-aws-eks"
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
-  vpc_id          = data.aws_vpc.vpc[0].id
+  vpc_id          = module.vpc.vpc_id
 
-  subnet_ids = var.vpc_create && (length(flatten(module.vpc.*.private_subnets)) == 0) ? module.vpc.private_subnets : flatten([for subnets in data.aws_subnets.cluster_subnet_set : tolist(subnets.ids)])
-
+  subnet_ids = module.vpc.private_subnets
   cluster_encryption_config = [
     {
       provider_key_arn = aws_kms_key.eks.arn
@@ -59,7 +25,7 @@ module "eks" {
   ]
 
   eks_managed_node_group_defaults = {
-    subnets              = flatten([for subnets in data.aws_subnets.node_subnet_set : tolist(subnets.ids)])
+    subnets              = module.vpc.public_subnets
     asg_max_size         = var.node_group_max_size
     asg_min_size         = var.node_group_min_size
     asg_desired_capacity = var.node_group_desired_capacity
--- 03_helm_ingress_nginx.tf
+++ 03_helm_ingress_nginx.tf
@@ -25,7 +25,7 @@ data "aws_route53_zone" "dns_zone" {
 
 resource "aws_route53_record" "rancher_cluster_ingress" {
   depends_on = [data.kubernetes_service.ingress_nginx_service]
-  zone_id    = data.aws_route53_zone.dns_zone[0].zone_id
+  zone_id    = module.zones.route53_zone_zone_id[0]
   name       = local.full_domain
   type       = "CNAME"
   records    = [data.kubernetes_service.ingress_nginx_service.status.0.load_balancer[*].ingress[0].hostname]
