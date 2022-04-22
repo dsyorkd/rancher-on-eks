d262f80b125f8cea5ce7d05dbb81f7f3630e623a
Spencer York <syork@yorkserv.com>
2022-04-21 21:34:01 -0500

    updating index

--- 01_eks.tf
+++ 01_eks.tf
@@ -14,9 +14,9 @@ module "eks" {
   source          = "github.com/terraform-aws-modules/terraform-aws-eks"
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
-  vpc_id          = module.vpc.vpc_id
+  vpc_id          = module.vpc[0].vpc_id
 
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc[0].private_subnets
   cluster_encryption_config = [
     {
       provider_key_arn = aws_kms_key.eks.arn
@@ -25,7 +25,7 @@ module "eks" {
   ]
 
   eks_managed_node_group_defaults = {
-    subnets              = module.vpc.public_subnets
+    subnets              = module.vpc[0].public_subnets
     asg_max_size         = var.node_group_max_size
     asg_min_size         = var.node_group_min_size
     asg_desired_capacity = var.node_group_desired_capacity
